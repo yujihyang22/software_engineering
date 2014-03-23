4e1340897153e941d9737243ef41d1b9f470cf2e
jihyang.yu <yujihyang22@naver.com>
2014-03-23 05:42:34 -0700

    to git hub code2

---
+++ code2.cpp
@@ -0,0 +1,86 @@
+<<<<<<< HEAD
+-#include <iostream>
+-using namespace std;
+-
+-
+-int main()
+-{
+-       char letters[256];
+-       int change;
+-
+-
+-       //modify
+-       cout<<"hello. this is jihyng world! welcome"<<endl;
+-       /////////
+-
+-       cout<<"Enter the string : ";
+-       cin.getline(letters,256);
+-       
+-       for(int i=0; i<256; i++)
+-       {
+-               if(letters[i]>='A' && letters[i]<='Z')
+-               {
+-                       change = letters[i] - 'A';
+-                       letters[i] = 'a' + change;
+-               }
+-
+-               else if(letters[i]>='a' && letters[i]<='z')
+-               {
+-                       change = letters[i] - 'a';
+-                       letters[i] = 'A' + change;
+-               }
+-       }
+-
+-       cout<<"Result : "<<letters<<endl;
+-
+
+	//modify2
+-       cout<<"thank you!!! bye"<<endl;
+-       /////////
+
+-       return 0;
+-}
+=======
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	char letters[256];
+	int change;
+
+
+	//++
+	cout<<" how to use git and github? "<<endl;
+	//
+	
+
+	//modify
+	cout<<"hello. this is jihyng world! welcome"<<endl;
+	/////////
+
+	cout<<"Enter the string : ";
+	cin.getline(letters,256);
+	
+	for(int i=0; i<256; i++)
+	{
+		if(letters[i]>='A' && letters[i]<='Z')
+		{
+			change = letters[i] - 'A';
+			letters[i] = 'a' + change;
+		}
+
+		else if(letters[i]>='a' && letters[i]<='z')
+		{
+			change = letters[i] - 'a';
+			letters[i] = 'A' + change;
+		}
+	}
+
+	cout<<"Result : "<<letters<<endl;
+
+	return 0;
+}
+>>>>>>> new
+
